c1d6a55e3631a81f3469839d08d83cf3d1084c53
Jonathan Brull Schroeder <jonathans@avalith.net>
2021-01-24 15:50:03 -0300

    Se agregan bd/getProfile , middleware checkJWT.go y routers/viewProfile

--- middlewares/checkJWT.go
+++ middlewares/checkJWT.go
@@ -2,19 +2,21 @@ package middlewares
 
 import (
 	"net/http"
+
+	"github.com/jonathanbs9/tweety/routers"
 )
 
 // ValidateJWT func => permite validar el JWT que nos viene en la petición
-func ValidateJWT(next http.HandlerFunc) http.HandlerFunc{
+func ValidateJWT(next http.HandlerFunc) http.HandlerFunc {
 	//funcion anónmia
-	return func(w http.ResponseWriter, r *http.Request){
+	return func(w http.ResponseWriter, r *http.Request) {
 		// Va a devolver 4 params.
 		_, _, _, err := routers.ProcessToken(r.Header.Get("Authorization"))
-		if  err != nil {
-			http.Error(w, "error en el token!"+ err.Error(), http.StatusBadRequest)
-			return 
+		if err != nil {
+			http.Error(w, "error en el token!"+err.Error(), http.StatusBadRequest)
+			return
 		}
 		// le pasamos 2 objetos para la proximo eslabon de la cadena
-		next.ServeHTTP(w, )
-	})
+		next.ServeHTTP(w, r)
+	}
 }
