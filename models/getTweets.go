a9b21a9f83580f79bf0568c9d7eed0c4649ab141
Jonathan Brull Schroeder <jonathans@avalith.net>
2021-01-28 15:42:06 -0300

    Fix models/getTweets => not compatible with reflect.Struct.Tag.Get: suspicious space

--- models/getTweets.go
+++ models/getTweets.go
@@ -8,8 +8,8 @@ import (
 
 // GetTweets => Estructura de devolución de un tweet.
 type GetTweets struct {
-	ID      primitive.ObjectID `bson:"_id" json:"_id, omitempty"`
-	UserID  string             `bson:"user_id" json:"user_id, omitempty"`
-	Message string             `bson:"message" json:"message, omitempty"`
-	Date    time.Time          `bson:"date" json:"date, omitempty"`
+	ID      primitive.ObjectID `bson:"_id" json:"_id,omitempty"`
+	UserID  string             `bson:"user_id" json:"user_id,omitempty"`
+	Message string             `bson:"message" json:"message,omitempty"`
+	Date    time.Time          `bson:"date" json:"date,omitempty"`
 }
